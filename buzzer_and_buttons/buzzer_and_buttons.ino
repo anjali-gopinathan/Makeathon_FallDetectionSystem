9ab7343c78f56ddbffe710cf5d2704d1a7d2b5ac
Enrique Perez <perezenr@usc.edu>
2021-04-19 11:18:13 -0700

    added gyro code

--- buzzer_and_buttons/buzzer_and_buttons.ino
+++ buzzer_and_buttons/buzzer_and_buttons.ino
@@ -22,12 +22,18 @@ void setup() {
 
 void loop() {
 
+  //read gyro values
+  sensors_event_t gyro;
+  lsm6ds33.getEvent(NULL, &gyro, NULL);
+  float x = gyro.gyro.x * SENSORS_RADS_TO_DPS;
+  float y = gyro.gyro.y * SENSORS_RADS_TO_DPS;
+
   //read button values
   emergencySystem.readButtons();
   uint8_t buttonVals = emergencySystem.justPressedButtons();
   touchSensor = digitalRead(touchSensorPin);
   //if A is pressed
-  if (buttonVals == 65 || touchSensor == LOW){
+  if (buttonVals == 65 || touchSensor == LOW || x > 50 || y > 50){
     bool disableBuzzer = 0;
 
     //while B button is not pressed
